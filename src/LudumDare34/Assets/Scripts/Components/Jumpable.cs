5167a248c1e58c68ba2340e2ccbe5971af296ca4
harjup <pikmin@gmail.com>
2015-12-12 12:14:52 -0500

    Partially implement jump physics

---
+++ src/LudumDare34/Assets/Scripts/Components/Jumpable.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Jumpable : MonoBehaviour
+{
+    public float FloorPosition = .5f;
+    public float Gravity = 30f;
+    public float InitialJumpDistance = .25f;
+    public float InitialVelocity = 20f;
+    public float HeldButtonAmount = 5f;
+
+    public enum JumpButton
+    {
+        Undefined,
+        LeftArrow,
+        RightArrow
+    }
+
+    public JumpButton Button { get; set; }
+
+    private JumpPhysics _jumpPhysics;
+
+	void Start ()
+	{
+        _jumpPhysics = new JumpPhysics(FloorPosition, InitialJumpDistance, Gravity, InitialVelocity);
+	}
+
+    void Update()
+    {
+        _jumpPhysics._heldButtonAmount = HeldButtonAmount;
+        _jumpPhysics._floorPosition = FloorPosition;
+        _jumpPhysics._gravity = Gravity;
+        _jumpPhysics._initialVelocty = InitialVelocity;
+        _jumpPhysics._initialJumpDistance = InitialJumpDistance;
+
+        var jumpButtonPressed = Input.GetKey(KeyCode.LeftArrow);
+
+        _jumpPhysics.JumpButtonPressed(jumpButtonPressed);
+	    _jumpPhysics.Tick(Time.deltaTime);
+
+        transform.position = transform.position.SetY(_jumpPhysics.YPos); 
+	}
+}
+
+public static class Vector3Extensions
+{
+    public static Vector3 SetY(this Vector3 position, float val)
+    {
+        return new Vector3(
+            position.x,
+            val,
+            position.z);
+    }
+}
---
+++ src/LudumDare34/Assets/Scripts/Utils/JumpPhysics.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class JumpPhysics
+{
+    public enum State
+    {
+        Undefined,
+        Ground,
+        Air
+    }
+
+
+    private State _state;
+    public float _floorPosition;
+    private float _ypos;
+
+    public float _initialVelocty = 20f;
+    public float _gravity = 30f;
+    public float _initialJumpDistance = 2.5f;
+    private float _velocity = 0f;
+
+    public float _heldButtonAmount = 3f;
+
+
+    public float YPos {get { return _ypos; }}
+
+    private bool _jumpButtonPressed = false;
+
+
+    public JumpPhysics(float floorPosition, float initialJumpDistance, float initialJumpStrength, float gravity)
+    {
+        _state = State.Ground;
+
+        _floorPosition = floorPosition;
+        _ypos = _floorPosition;
+    }
+
+    public void JumpButtonPressed(bool value)
+    {
+        _jumpButtonPressed = value;
+    }
+
+    public void Tick(float deltatime)
+    {
+        switch (_state)
+        {
+            case State.Ground:
+                // TODO: Cooldown so you can't just hold the button
+                if (_jumpButtonPressed)
+                {
+                    Jump();
+                }
+                break;
+            case State.Air:
+
+                //  Add velocity from button being pressed for x available seconds
+                //  Decrease velocity by gravity
+                //  If going to be under the floor...
+                //  Set state to ground
+                //  Set velocity to 0
+
+                if (_jumpButtonPressed)
+                {
+                    _velocity += _heldButtonAmount * deltatime;
+                }
+
+                _velocity -= _gravity*deltatime;
+                _ypos += (deltatime*_velocity);
+                if (_ypos < _floorPosition)
+                {
+                    Land();
+                }
+
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void Land()
+    {
+        _state = State.Ground;
+        _velocity = 0f;
+        _ypos = _floorPosition;
+    }
+
+
+    private void Jump()
+    {
+        _state = State.Air;
+        _velocity = _initialVelocty;
+        _ypos += _initialJumpDistance;
+        // Set initial velocity
+        // Set jump button timer (how long it can be held)
+    }
+
+}
