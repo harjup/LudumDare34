bab240c0ce1dc663098e63d02708adc9caa2f06e
harjup <pikmin@gmail.com>
2015-12-13 20:04:12 -0500

    Add assembly line to combat scene, add items to belt

---
+++ src/LudumDare34/Assets/src/Scripts/Components/BeltItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+
+public class BeltItem : MonoBehaviour
+{
+
+    public List<Sprite> sprites;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites.AsRandom().First();
+
+        transform.DOMoveX(-18, 12f)
+            .SetEase(Ease.Linear)
+            .OnComplete(() => {Destroy(this.gameObject);});
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ src/LudumDare34/Assets/src/Scripts/Components/PrefabSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    public GameObject _target;
+    public float Min;
+    public float Max;
+
+    public void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    public IEnumerator SpawnObjects()
+    {
+        var nextDelay = Random.Range(Min, Max);
+
+        while (true)
+        {
+            Instantiate(_target, transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(nextDelay);
+        }
+    }
+
+}
